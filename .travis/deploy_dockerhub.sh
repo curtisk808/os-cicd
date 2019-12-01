803ff7b89d271771269ae56ad85d3562ed0c40cd
curtisk808 <gbaamk@gmail.com>
2019-12-01 21:19:40 +0000

    deploy_dockerhub.sh

--- .travis/deploy_dockerhub.sh
+++ .travis/deploy_dockerhub.sh
@@ -6,5 +6,9 @@ else
     TAG="$TRAVIS_BRANCH"
 fi
 sudo docker build -f Dockerfile -t $TRAVIS_REPO_SLUG:$TAG .
+sudo docker images
+echo docker_user is $DOCKER_USER
+echo docker_pass is $DOCKER_PASS
+
 sudo docker push $TRAVIS_REPO_SLUG:$TAG
 
