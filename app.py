21035b91a53522b3e6534eb60f296d44a698038f
curtisk808 <gbaamk@gmail.com>
2019-12-02 23:09:10 +0000

    yo

--- app.py
+++ app.py
@@ -6,11 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def generate_buzz():
-<<<<<<< HEAD
-    f = open("website.txt","r")
-=======
     f=open("website.txt","r")
->>>>>>> e3e014d6aa57c40b83b22facada4dc1121bba6f9
     page = f.read()
     return page
 
